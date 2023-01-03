d21bd4efe1205db78135007d62e6f6e1a4e66cc0
morgane-marechal <morgane.marechal@laplateforme.io>
2023-01-03 13:05:26 +0100

    ajout delete() et update()

--- User.php
+++ User.php
@@ -4,7 +4,7 @@ session_start();
 <?php 
 echo "Cette page fonctionne. <br>";
 $mysqli = mysqli_connect('localhost','root','','classes');
-echo $_SESSION['login']."<br>";
+echo "Login de session: ".$_SESSION['login']."<br>";
 
 // Check connection
 if (mysqli_connect_errno()) {
@@ -54,28 +54,28 @@ class User{
         
     }
 
-    function connect($login, $password){
-        echo "hello";
+    public function connect($login, $password){
         global $mysqli;
         $this->password=$password;
         $this->login = $login;
         $request="SELECT count(*) FROM utilisateurs where login = '$login' and password = '$password'";
         $exec_requete = mysqli_query($mysqli,$request);
         $reponse = mysqli_fetch_array($exec_requete);
-        //echo var_dump($reponse);
         $count = $reponse['count(*)'];
         if($count!=0){
-            echo "Bravo vous êtes connectés!<br>";
+            echo "Bravo vous êtes connectés!
+            <br> Si ce message s'affiche c'est que vous vous êtes connecté avec succès<br>";
             $_SESSION['login'] = $login;
             $_SESSION['email'] = $this->email;
             $_SESSION['firstname'] = $this->firstname;
             $_SESSION['lastname'] = $this->lastname;
+            echo "Voici vos identifiants de session: ".$login.", ".$this->email.", ".$this->firstname.", ".$this->lastname."<br>";
         }else{
             echo "Problème d'identifiant ou de mot de passe";
         }
     }
 
-    function disconnect(){
+    public function disconnect(){
         if (isset($_SESSION['login'])&& !empty($_SESSION['login'])){
             echo "Vous êtes déconnecté";
             return session_destroy();
@@ -83,7 +83,7 @@ class User{
         }
     }
 
-    function isConnected(){
+    public function isConnected(){
         // global $isConnected;
         // $isConnected=TRUE;
         if (isset($_SESSION['login'])){
@@ -92,23 +92,54 @@ class User{
             return $isConnected = TRUE;
         }
     }
+
+    public function delete(){
+        global $mysqli;
+        $delete="DELETE from utilisateurs WHERE login = '$this->login'";
+        mysqli_query($mysqli,$delete);
+        echo $this->login." supprimé";
+        session_destroy();
+    }
+
+    public function update($newlogin, $password, $email, $firstname,$lastname){
+        global $mysqli;
+        $login=$_SESSION['login'];
+        $this->password=$password;
+        $this->login = $newlogin;
+        $this->email = $email;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $sqlupdate = "UPDATE utilisateurs SET login = '$newlogin', password = '$password', email = '$email', firstname = '$firstname', lastname = '$lastname' WHERE login = '$login'";
+        mysqli_query($mysqli,$sqlupdate);
+        if ($mysqli->query($sqlupdate) === TRUE) {
+            echo "Vous avez amise à jour votre profil.<br>";
+            }else {
+            echo "Erreur: " . $sqlupdate . "
+            " . $mysqli->error;
+            }
+    }
   
     
 }
 
 
-$lucia= new User("Lucia","luci@gmail.com", "Lucie", "Alamond");
+$update= new User("Lucia","luci@gmail.com", "Lucie", "Alamond");
 //$newuser->register("Lucia","azety","luci@gmail.com", "Lucie", "Alamond");
 //echo var_dump($lucia);
 //$lucia->register("Lucia","azerty","luci@gmail.com", "Lucie", "Alamond");
 //$kevin->register("Kevin","azerty2","kev@gmail.com", "Kevin", "Bond");
-$barnabe= new User("Barnabe","nanrabar@gmail.com", "Bar Na Bay", "Glouglou");
-$yusu= new User("Yusuka","yusss@gmail.com", "Yusune", "Mariko");
+//$barnabe= new User("Barnabe","nanrabar@gmail.com", "Bar Na Bay", "Glouglou");
 //$yusu->register("Yusuka","123","yusss@gmail.com", "Yusune", "Mariko");
 //echo var_dump($barnabe);
-//$yusu->connect("Yusuka", "123");
+$update->connect("Lucianna","123CF");
+//$update->update("Lucianna", "123CF", "lucia@gmail.com","Lucianna","Alamonde");
 
 //echo $barnabe->isConnected();
-$barnabe->disconnect();
+//$barnabe->disconnect();
+// if ($isConnected===TRUE){
+//     echo "<br>La variable isConnected en global fonctionne, elle renvoie $isConnected";
+// }
+// $deleteUser= new User("Kevin","kev@gmail.com", "Kevin", "Bond");
 
+//$deleteUser->delete();
 ?>

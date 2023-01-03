77636e44291c6789b3a72c38583fec7c713bb3ea
morgane-marechal <morgane.marechal@laplateforme.io>
2023-01-03 10:45:49 +0100

    ajout disconnect(), connect()

--- User.php
+++ User.php
@@ -1,6 +1,10 @@
+<?php
+session_start();
+?>
 <?php 
 echo "Cette page fonctionne. <br>";
 $mysqli = mysqli_connect('localhost','root','','classes');
+echo $_SESSION['login']."<br>";
 
 // Check connection
 if (mysqli_connect_errno()) {
@@ -10,48 +14,84 @@ if (mysqli_connect_errno()) {
     echo "Vous êtes connecté à la base de donnée. <br>";
 }
 
+echo "<br><br><br>";
+
+
 class User{
     private $id;
     public $login, $email, $firstname, $lastname;
 
-    /*function __construct($login,  $email, $firstname, $lastname) {
+    function __construct($login,  $email, $firstname, $lastname) {
         $this->login = $login;
         $this->email = $email;
         $this->firstname = $firstname;
         $this->lastname = $lastname;
-    }*/
+    }
 
-    function register($login, $password, $email, $firstname,$lastname){
+    public function register($login, $password, $email, $firstname,$lastname){
         global $mysqli;
         $this->password=$password;
         $this->login = $login;
         $this->email = $email;
         $this->firstname = $firstname;
         $this->lastname = $lastname;
-
-        $newuser = "INSERT INTO utilisateurs ( login, password, email, firstname, lastname)
-        VALUES( '$login','$password', '$email', '$firstname', '$lastname')";
-        if ($mysqli->query($newuser) === TRUE) {
-            echo "Vous avez ajouté un utilisateur avec succés.<br>";
-            }else {
-                echo "Erreur: " . $newuser . "
-                " . $mysqli->error;
-                }
+        $check_login = "SELECT count(*) FROM utilisateurs where login = '$login'";
+        $exec_requete = mysqli_query($mysqli,$check_login);
+        $reponse = mysqli_fetch_array($exec_requete);
+        $count = $reponse['count(*)'];
+        if ($count!=0){
+            echo "<p>Ce login est déjà pris, veuillez en choisir un autre!</p>";}
+            else{
+                $newPeople = "INSERT INTO utilisateurs ( login, password, email, firstname, lastname)
+                VALUES( '$login','$password', '$email', '$firstname', '$lastname')";
+                    if ($mysqli->query($newPeople) === TRUE) {
+                    echo "Vous avez ajouté un utilisateur avec succés.<br>";
+                    }else {
+                    echo "Erreur: " . $newPeople . "
+                    " . $mysqli->error;
+                    }
+            }
+        
     }
 
     function connect($login, $password){
+        echo "hello";
         global $mysqli;
-        $request="SELECT * FROM utilisateurs where login = '$login'and password= '$password'";
-        
-        if ($mysqli->query($request) === TRUE){
-            echo "Bravo vous êtes connectés!";
+        $this->password=$password;
+        $this->login = $login;
+        $request="SELECT count(*) FROM utilisateurs where login = '$login' and password = '$password'";
+        $exec_requete = mysqli_query($mysqli,$request);
+        $reponse = mysqli_fetch_array($exec_requete);
+        //echo var_dump($reponse);
+        $count = $reponse['count(*)'];
+        if($count!=0){
+            echo "Bravo vous êtes connectés!<br>";
+            $_SESSION['login'] = $login;
+            $_SESSION['email'] = $this->email;
+            $_SESSION['firstname'] = $this->firstname;
+            $_SESSION['lastname'] = $this->lastname;
         }else{
             echo "Problème d'identifiant ou de mot de passe";
         }
-       
-
     }
 
+    function disconnect(){
+        if (isset($_SESSION['login'])&& !empty($_SESSION['login'])){
+            echo "Vous êtes déconnecté";
+            return session_destroy();
+
+        }
+    }
+
+    function isConnected(){
+        // global $isConnected;
+        // $isConnected=TRUE;
+        if (isset($_SESSION['login'])){
+            echo "Quelqu'un est connecté à cette page. Il s'agit de ".$_SESSION['login']."<br>";
+            global $isConnected;
+            return $isConnected = TRUE;
+        }
+    }
   
     
 }
@@ -59,12 +99,16 @@ class User{
 
 $lucia= new User("Lucia","luci@gmail.com", "Lucie", "Alamond");
 //$newuser->register("Lucia","azety","luci@gmail.com", "Lucie", "Alamond");
-echo var_dump($lucia);
-$lucia->register("Lucia","azerty","luci@gmail.com", "Lucie", "Alamond");
+//echo var_dump($lucia);
+//$lucia->register("Lucia","azerty","luci@gmail.com", "Lucie", "Alamond");
+//$kevin->register("Kevin","azerty2","kev@gmail.com", "Kevin", "Bond");
+$barnabe= new User("Barnabe","nanrabar@gmail.com", "Bar Na Bay", "Glouglou");
+$yusu= new User("Yusuka","yusss@gmail.com", "Yusune", "Mariko");
+//$yusu->register("Yusuka","123","yusss@gmail.com", "Yusune", "Mariko");
+//echo var_dump($barnabe);
+//$yusu->connect("Yusuka", "123");
 
-$kevin= new User("Kevin","kev@gmail.com", "Kevin", "Bond");
-echo var_dump($kevin);
-$kevin->register("Kevin","azerty2","kev@gmail.com", "Kevin", "Bond");
+//echo $barnabe->isConnected();
+$barnabe->disconnect();
 
-//$kevin->connect("Kevin", "azerty2");
 ?>
